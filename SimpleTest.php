305f0a6f045536d2c36cfee91678f0fbdebf8511
Hafidz Fadhillah Febrianto <63035660+Hafidz1121@users.noreply.github.com>
2023-10-19 14:54:46 +0700

    Update Penjelasan Baris Kode

--- SimpleTest.php
+++ SimpleTest.php
@@ -1,14 +1,23 @@
 <?php
+    // Melakukan import / pemanggilan file dan sintax yang di perlukan
     use PHPUnit\Framework\TestCase;
     require_once "WordCount.php";
 
+    // Merupakan sebuah class dengan extends / pengimplementasian TestCase
     class SimpleTest extends TestCase {
+
+        // Adalah sebuah fungsi 
         public function testCountWords() {
+            // Penginisialisasian dengan nama class WordCount()
             $Wc = new WordCount();
 
+            // Pendeklarasian string ke dalam variabel $TestSentences
             $TestSentence = "My Name Is Joko";
+
+            // Pendeklarasian sebuah varibel dengan memanggil fungsi countWord untuk menghitung kata
             $WordCount = $Wc->countWords($TestSentence);
 
+            // Membandingkan 2 buah variabel apakah sama atau tidak
             $this->assertEquals(4, $WordCount);
         }
     }
--- WordCount.php
+++ WordCount.php
@@ -1,6 +1,12 @@
 <?php
+    // Sebuah Class bernama WordCount
     class WordCount {
+
+        // Sebuah fungsi bernama countWords dengan parameter $sentence
         public function countWords($sentence) {
+            // Mengembalikan nilai dengan menggunakan sintax php count untuk menghitung
+            // Dan sintax php explode untuk memisahkan String dengan parameter pemisahnya dalam hal ini adalah spasi (" ")
+            // Kemudian memasukan ke dalam variabel $sentence
             return count(explode(" ", $sentence));
         }
     }
